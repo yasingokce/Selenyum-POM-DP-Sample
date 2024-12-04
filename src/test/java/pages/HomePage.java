37ea0f683e8c33282696de843e30e2c55c162c32
yasin gökçe şahin <48259029+yasingokce@users.noreply.github.com>
2024-12-04 23:35:08 +0300

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class HomePage extends Pages {
 
     public final String searchPanel = "//input[@id='search-input']";
-    public final String findString = "insan pisikolojisi";
+    public final String findString = "yazılım kitabı";
     public final String ipLocator = "//ul[@class='product-grid small jcarousel-skin-opencart']//img[@alt='İnsan Psikolojisi']";  
     public final String packageString = "#button-cart > span";
     public final String basket = "#cart-items";
