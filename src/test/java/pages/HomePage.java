e2829db6a08f601b3c7c6b92fc5a2610d5ab580a
yasin gökçe şahin <48259029+yasingokce@users.noreply.github.com>
2024-12-04 23:35:53 +0300

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,7 +7,7 @@ public class HomePage extends Pages {
 
     public final String searchPanel = "//input[@id='search-input']";
     public final String findString = "yazılım kitabı";
-    public final String ipLocator = "//ul[@class='product-grid small jcarousel-skin-opencart']//img[@alt='İnsan Psikolojisi']";  
+    public final String ipLocator = "//ul[@class='product-grid small jcarousel-skin-opencart']//img[@alt='']";  
     public final String packageString = "#button-cart > span";
     public final String basket = "#cart-items";
     public final String basketbutton = "#js-cart";
